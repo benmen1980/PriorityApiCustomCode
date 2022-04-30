2883b174053e5e0f19e7f031ff1f78b3293f4450
Roy <benmen1@gmail.com>
2022-05-01 00:53:08 +0300

    fix family-code file change function name to simply...

--- inc/helbitz.php
+++ inc/helbitz.php
@@ -2,12 +2,12 @@
 add_filter('simply_request_data','manipulate_data');
 function manipulate_data($data){
     $items = [];
-    foreach($data['ORDERITEMS_SUBFORM'] as $item ){
+    foreach($data['EINVOICEITEMS_SUBFORM'] as $item ){
         if($item['PARTNAME']==''){
             $item['PARTNAME'] =  '000';
         }
         $items[] = $item;
     }
-    $data['ORDERITEMS_SUBFORM'] = $items;
+    $data['EINVOICEITEMS_SUBFORM'] = $items;
     return $data;
 }
