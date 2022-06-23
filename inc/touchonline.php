45fd5fed47d4a80620dbbc93bd965e930b2e614c
Ruth-Z-SimpleCT <ruth.z@roi-holdings.com>
2022-06-23 13:54:27 +0300

    add touchonline

---
+++ inc/touchonline.php
@@ -0,0 +1,27 @@
+<?php
+add_filter('simply_request_data','simply_func');
+function simply_func($data){
+    $order_id = $data['BOOKNUM'];
+    $order = new \WC_Order($order_id);
+    //$cashback = get_post_meta($order_id,'_wallet_cashback',true);
+    $cashback = get_order_partial_payment_amount( $order_id ); // update 14.07.2021
+    if($cashback>0){
+        $data['ORDERITEMS_SUBFORM'][] = [
+            'BARCODE' => 'CB1234', // change to other item
+
+            'VATPRICE' => -1* $cashback,
+            'TQUANT'   => -1,
+
+        ];
+    }
+
+
+    return $data;
+}
+add_filter('simply_set_priority_sku_field','simply_set_priority_sku_field_func');
+function simply_set_priority_sku_field_func($fieldname)
+{
+    $fieldname="BARCODE";
+    return $fieldname;
+}
+?>
