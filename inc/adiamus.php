b811dc031fe95e75944e49c9e2d2221143b4dfc2
Ruth-Z-SimpleCT <ruth.z@roi-holdings.com>
2022-07-11 15:24:38 +0300

    update adiamus

--- inc/adiamus.php
+++ inc/adiamus.php
@@ -1,8 +1,9 @@
 <?php
-add_filter('simply_syncPricePriority', 'simply_syncPricePriority_func');
-function simply_syncPricePriority_func($data)
+add_filter('simply_syncItemsPriority_data','simply_selectPrice_func');
+add_filter('simply_syncPricePriority', 'simply_selectPrice_func');
+function simply_selectPrice_func($data)
 {
-    $data['select']='PARTNAME,BASEPLPRICE,VATPRICE,BARCODE,WSPLPRICE';
+    $data['select'].=',WSPLPRICE';
     return $data;
 }
 ?>
