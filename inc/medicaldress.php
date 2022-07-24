3237bf51776bad0099f71fb6a73733148ab93de9
Ruth-Z-SimpleCT <ruth.z@roi-holdings.com>
2022-07-24 15:40:00 +0300

    update medicaldress

--- inc/medicaldress.php
+++ inc/medicaldress.php
@@ -3,13 +3,12 @@ add_filter('simply_request_data', 'simply_request_data_func');
 function simply_request_data_func($data)
 {
     $item_warehouse = '75';
-    $data = [
-        'DCODE' => 'web',
-        'DETAILS' => '01699985',
-        'UNI_ORDTYPE' => 'B',
-        'UNI_DUEDATE' => date('Y-m-d'),
-        'UFLR_ORDERRCVCODE' => '8'
-    ];
+    $data['DCODE'] = 'web';
+    $data['DETAILS'] = '01699985';
+    $data['UNI_ORDTYPE'] = 'B';
+    $data['UNI_DUEDATE'] = date('Y-m-d');
+    $data['UFLR_ORDERRCVCODE'] = '8';
+
     for ($i = 0; $i < count($data['ORDERITEMS_SUBFORM']); $i++)
         $data['ORDERITEMS_SUBFORM'][$i] = [
             'UNI_ORDTYPE' => 'B',
