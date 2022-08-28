0caf39444b1b50cf8f251985d427bd9cf39d635a
Ruth-Z-SimpleCT <ruth.z@roi-holdings.com>
2022-08-28 15:50:07 +0300

    update lia

--- inc/lia.php
+++ inc/lia.php
@@ -5,3 +5,10 @@ function simply_request_data_func($data)
     $data['CASHNAME'] = '102';
     return $data;
 }
+
+add_filter('simply_sync_inventory_priority', 'simply_sync_inventory_priority_func');
+function simply_sync_inventory_priority($item)
+{
+    $item['stock'] = $item['LOGCOUNTERS_SUBFORM'][0]['BALANCE'] - 2;
+    return $item;
+}
