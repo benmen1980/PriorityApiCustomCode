3e9e6a16d433c568a505d43ab75eb338122ed027
Ruth-Z-SimpleCT <ruth.z@roi-holdings.com>
2022-08-28 17:03:32 +0300

    update lia

--- inc/lia.php
+++ inc/lia.php
@@ -7,7 +7,7 @@ function simply_request_data_func($data)
 }
 
 add_filter('simply_sync_inventory_priority', 'simply_sync_inventory_priority_func');
-function simply_sync_inventory_priority($item)
+function simply_sync_inventory_priority_func($item)
 {
     $item['stock'] = $item['LOGCOUNTERS_SUBFORM'][0]['BALANCE'] - 2;
     return $item;
