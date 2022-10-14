fec08a2268018ddfbee92a26547cd18d1a76e91e
Roy <benmen1@gmail.com>
2022-10-14 11:45:15 +0300

    add artaste to plugin 3

---
+++ inc/artaste.php
@@ -0,0 +1,33 @@
+<?php
+use PriorityWoocommerceAPI\WooAPI;
+
+
+add_filter('simply_request_data', 'simply_func');
+function simply_func($data)
+
+{
+    // CURDATE = PDATE
+    $data['PDATE'] = $data['CURDATE'];
+    unset($data['CURDATE']);
+    // ORDERSTEXT_SUBFORM replace with CPROFTEXT
+    $data = WooAPI::instance()->change_key($data,'ORDERSTEXT_SUBFORM','CPROFTEXT_SUBFORM');
+    // CPROFCONT
+    $data = WooAPI::instance()->change_key($data,'ORDERSCONT_SUBFORM','CPROFCONT_SUBFORM');
+    // ORDER ITEMS REPLACE CPROFITEMS
+    $data = WooAPI::instance()->change_key($data,'ORDERITEMS_SUBFORM','CPROFITEMS_SUBFORM');
+
+    // replace cprofitems fields
+    $items = [];
+    foreach ($data['CPROFITEMS_SUBFORM'] as $item){
+        unset($item['DUEDATE']);
+        $item['VPRICE'] = $item['VATPRICE']/$item['TQUANT'];
+        unset($item['VATPRICE']);
+        $items[] = $item;
+    }
+    $data['CPROFITEMS_SUBFORM']  = $items;
+    // PAYMENTDEF
+    unset($data['PAYMENTDEF_SUBFORM']);
+
+    return $data;
+
+}
