10a29015933d15f2ac8289df24d31bcfeeb18c14
Roy <benmen1@gmail.com>
2022-10-14 12:04:25 +0300

    artaste get the pq number and post it in the otc

--- inc/artaste.php
+++ inc/artaste.php
@@ -6,28 +6,33 @@ add_filter('simply_request_data', 'simply_func');
 function simply_func($data)
 
 {
-    // CURDATE = PDATE
-    $data['PDATE'] = $data['CURDATE'];
-    unset($data['CURDATE']);
-    // ORDERSTEXT_SUBFORM replace with CPROFTEXT
-    $data = WooAPI::instance()->change_key($data,'ORDERSTEXT_SUBFORM','CPROFTEXT_SUBFORM');
-    // CPROFCONT
-    $data = WooAPI::instance()->change_key($data,'ORDERSCONT_SUBFORM','CPROFCONT_SUBFORM');
-    // ORDER ITEMS REPLACE CPROFITEMS
-    $data = WooAPI::instance()->change_key($data,'ORDERITEMS_SUBFORM','CPROFITEMS_SUBFORM');
-
-    // replace cprofitems fields
-    $items = [];
-    foreach ($data['CPROFITEMS_SUBFORM'] as $item){
-        unset($item['DUEDATE']);
-        $item['VPRICE'] = $item['VATPRICE']/$item['TQUANT'];
-        unset($item['VATPRICE']);
-        $items[] = $item;
+    if($data['doctype']=='ORDERS') {
+        // CURDATE = PDATE
+        $data['PDATE'] = $data['CURDATE'];
+        unset($data['CURDATE']);
+        // ORDERSTEXT_SUBFORM replace with CPROFTEXT
+        $data = WooAPI::instance()->change_key($data, 'ORDERSTEXT_SUBFORM', 'CPROFTEXT_SUBFORM');
+        // CPROFCONT
+        $data = WooAPI::instance()->change_key($data, 'ORDERSCONT_SUBFORM', 'CPROFCONT_SUBFORM');
+        // ORDER ITEMS REPLACE CPROFITEMS
+        $data = WooAPI::instance()->change_key($data, 'ORDERITEMS_SUBFORM', 'CPROFITEMS_SUBFORM');
+        // replace cprofitems fields
+        $items = [];
+        foreach ($data['CPROFITEMS_SUBFORM'] as $item) {
+            unset($item['DUEDATE']);
+            $item['VPRICE'] = $item['VATPRICE'] / $item['TQUANT'];
+            unset($item['VATPRICE']);
+            $items[] = $item;
+        }
+        $data['CPROFITEMS_SUBFORM'] = $items;
+        // PAYMENTDEF
+        unset($data['PAYMENTDEF_SUBFORM']);
     }
-    $data['CPROFITEMS_SUBFORM']  = $items;
-    // PAYMENTDEF
-    unset($data['PAYMENTDEF_SUBFORM']);
-
+    if($data['doctype']=='EINVOICES') {
+        $id = $data['DETAILS'];
+        $CPROFNUM = get_post_meta($id,'priority_order_number',true);
+        $data['DETAILS'] = $CPROFNUM;
+        unset($data['IVDATE']);
+    }
     return $data;
-
 }
