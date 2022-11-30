12e14abffe3857d9235132543da67f89951ac04d
Roy <benmen1@gmail.com>
2022-11-30 23:27:15 +0200

    home pro reference

--- inc/home-pro.php
+++ inc/home-pro.php
@@ -4,3 +4,12 @@ function simply_syncCustomer($data){
 unset($data['EDOCUMENTS']);
 return $data;
 }
+add_filter('simply_request_data', 'simply_request_data_func');
+function simply_request_data_func($data)
+{
+    $id_order = $data["orderId"];
+    $order = new \WC_Order($id_order);
+    $data['REFERENCE'] = $data['BOOKNUM'];
+    unset($data['BOOKNUM']);
+    return $data;
+}
