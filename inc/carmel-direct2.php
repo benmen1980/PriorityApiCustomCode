4f6f6699c9d9c0681947803aa95517e4e1fdd299
unknown <benmen1@gmail.com>
2022-12-07 09:17:58 +0200

    add agentcode to carmel direct

--- inc/carmel-direct2.php
+++ inc/carmel-direct2.php
@@ -5,8 +5,13 @@ function simply_syncCustomer_func($json)
 
     unset($json['CUSTNAME']);
     $json['CTYPECODE'] = '10';
-    $json['AGENTCODE'] = '503';
+   // $json['AGENTCODE'] = '503';
    // unset($json['STATEA']);
     return $json;
 
+}
+add_filter('simply_request_data','manipulate_order');
+function manipulate_order($data){
+    $data['AGENTCODE'] = '503';
+    return $data;
 }
