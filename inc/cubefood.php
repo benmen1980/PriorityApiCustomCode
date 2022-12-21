cc7604087de216993e22b5ba86928c5d71f72164
unknown <benmen1980@gmail.com>
2022-12-21 11:13:48 +0200

    cube food sync ship class

--- inc/cubefood.php
+++ inc/cubefood.php
@@ -24,3 +24,23 @@ function simply_modify_long_text_func($data)
     }
     return $data;
 }
+
+if(!function_exists('simply_set_ship_class')) {
+    function simply_set_ship_class($product_id, $class_name)
+    {
+        // To get all the shipping classes
+        $shipping_classes = get_terms(array('taxonomy' => 'product_shipping_class', 'hide_empty' => false));
+        foreach ($shipping_classes as $shipping_class) {
+            if ($class_name == $shipping_class->name) {
+                // assign class to product
+                $product = wc_get_product($product_id); // Get an instance of the WC_Product Object
+                $product->set_shipping_class_id($shipping_class->term_id); // Set the shipping class ID
+                $product->save(); // Save the product data to database
+            }
+        }
+    }
+}
+
+add_action('simply_update_product_data',function($item){
+    simply_set_ship_class($item['product_id'],$item['SPEC19']);
+});
