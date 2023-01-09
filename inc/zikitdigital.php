ee0d6a2ed6d8bed572d4b6b190c0bfcb64584ac1
unknown <benmen1980@gmail.com>
2023-01-09 15:06:34 +0200

    zikit filter purchase order

--- inc/zikitdigital.php
+++ inc/zikitdigital.php
@@ -2,6 +2,6 @@
 
 add_filter('simply_syncInventoryPriority_data','simply_syncInventoryPriority_data');
 function simply_syncInventoryPriority_data($data){
-	$data['expand'] =  '$expand=LOGCOUNTERS_SUBFORM($expand=PARTAVAIL_SUBFORM),PARTBALANCE_SUBFORM';
+	$data['expand'] =  '$expand=LOGCOUNTERS_SUBFORM($expand=PARTAVAIL_SUBFORM($filter=TITLE eq \'הזמנות רכש\')),PARTBALANCE_SUBFORM';
 	return $data;
 }
