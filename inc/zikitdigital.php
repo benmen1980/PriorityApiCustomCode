40eace753ba06572eaa92c323be0f313c731be49
unknown <benmen1980@gmail.com>
2023-01-11 13:20:00 +0200

    zikit update role

--- inc/zikitdigital.php
+++ inc/zikitdigital.php
@@ -5,3 +5,16 @@ function simply_syncInventoryPriority_data($data){
 	$data['expand'] =  '$expand=LOGCOUNTERS_SUBFORM($expand=PARTAVAIL_SUBFORM($filter=TITLE eq \'הזמנות רכש\')),PARTBALANCE_SUBFORM';
 	return $data;
 }
+add_filter('simply_sync_priority_customers_to_wp','simply_sync_priority_customers_to_wp');
+function simply_sync_priority_customers_to_wp($user){
+//    $wp_user_object = new WP_User(\\\$user['user_id']);
+//    $wp_user_object -> set_role('');
+
+	if($user['STATDES'] == 'מוגבל')
+    {
+	    $wp_user_object = new WP_User($user['user_id']);
+        $wp_user_object -> set_role('');
+        //update_user_meta($user['user_id'], 'role', '');
+    }
+
+}
