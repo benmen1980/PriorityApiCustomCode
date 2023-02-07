1acab65719e76e7782d6fe9a9c14142e6083c516
unknown <benmen1980@gmail.com>
2023-02-07 15:02:16 +0200

    shalmon sync inventory without PO

--- inc/shalmon.php
+++ inc/shalmon.php
@@ -7,3 +7,9 @@ function simply_request_data_func($data)
     $data['CDES'] = $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
     return $data;
 }
+function simply_code_after_sync_inventory($product_id,$item)
+{
+	$stock = $item['LOGCOUNTERS_SUBFORM'][0]['BALANCE'];
+	$item['stock'] = $stock;
+	update_post_meta($product_id, '_stock', $stock);
+}
