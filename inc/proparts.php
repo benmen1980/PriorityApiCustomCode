b6b5b68fd0cd760bb2920a223e1c69c711cbbcd8
margalit-t <132431154+margalit-t@users.noreply.github.com>
2023-11-01 09:29:38 +0200

    update proparts file

--- inc/proparts.php
+++ inc/proparts.php
@@ -2,6 +2,12 @@
 
 use PriorityWoocommerceAPI\WooAPI;
 
+add_filter('simply_syncItemsPriority_data', 'simply_syncItemsPriority_data_func');
+function simply_syncItemsPriority_data_func($data)
+{
+    $data['select'] .= ',SUPDES';
+    return $data;
+}
 
 
 add_action('simply_update_product_data', function($item){
@@ -21,8 +27,11 @@ add_action('simply_update_product_data', function($item){
     if ( $the_query->have_posts() ) {
         while ( $the_query->have_posts() ) {
             $the_query->the_post();
-            $product_id = get_the_ID();
-            update_field('mutag', $product_id, $id);
+            $mutag_id = get_the_ID();
+            update_field('mutag', $mutag_id, $id); 
         }
-    }   
+    } else {
+        error_log("Manufacturer: '$manufacturer' not found.");
+    } 
+    
 });
