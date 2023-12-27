ce3b7674071a2808c32ef8f6ddef0d1c9eb05096
elicheva katz <elisheva.g@simplyct.co.il>
2023-12-27 14:12:51 +0200

    add change to bedulina file about warshname 50

--- inc/budulina.php
+++ inc/budulina.php
@@ -6,9 +6,16 @@ function simply_code_after_sync_inventory($product_id,$item){
 $desc = $item['EPARTDES'];
 $purchase_order = $item['LOGCOUNTERS_SUBFORM'][0]['PORDERS']; //הזמנות רכש
 $available_inventory = $item['LOGCOUNTERS_SUBFORM'][0]['BALANCE']; //מלאי זמין
-if ( $purchase_order > 0 && $available_inventory < 4 ) {
+//הזמנת רכש ממחסן 50
+$purchase_order_warshname_50 = $item['PARTBALANCE_SUBFORM'][0]['TBALANCE'];
+
+if ( ($purchase_order > 0 && $available_inventory < 4) || ($purchase_order_warshname_50 > 0) ) {
     update_post_meta($product_id, '_backorders', 'notify');
     update_post_meta($product_id, '_backorder_description', $desc);
+}
+else{
+    update_post_meta($product_id, '_backorders', 'no');
+    update_post_meta($product_id, '_backorder_description', '');
 }
 
 return null;
@@ -35,7 +42,8 @@ add_filter('simply_syncInventoryPriority_data', 'simply_syncInventoryPriority_da
 function simply_syncInventoryPriority_data_func($data)
 
 {
-
+    $expand = '$expand=LOGCOUNTERS_SUBFORM($select=DIFF,BALANCE,PORDERS),PARTBALANCE_SUBFORM($filter=WARHSNAME eq \'50\')';
+    $data['expand'] = $expand;
     $data['select'] = 'PARTNAME,EPARTDES';
 
     return $data;
