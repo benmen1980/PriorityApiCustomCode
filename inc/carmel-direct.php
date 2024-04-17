6dd69c194682c5fa4946197ad54b647a516eaf06
margalit-t <132431154+margalit-t@users.noreply.github.com>
2024-04-17 16:40:47 +0300

    update carmel direct

--- inc/carmel-direct.php
+++ inc/carmel-direct.php
@@ -24,7 +24,7 @@ function simply_func($data)
     // third function logic
     $id = $data['orderId'];
     $order = new \WC_Order($id);
-    if ($order->get_payment_method_title() !== 'PayPal') {
+    if ($order->get_payment_method_title() !== 'PayPal' && $order->get_payment_method_title() !== 'שובר BUYME -  GiftZone') {
         $order_cc_meta = $order->get_meta('_transaction_data');
         // data
         $credittype = $order_cc_meta['CardHebrewName'];
