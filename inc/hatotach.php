c43aa43a86a9b767b36012676100069c0185b97a
elicheva katz <elisheva.g@simplyct.co.il>
2025-07-17 10:06:27 +0300

    update clients

--- inc/hatotach.php
+++ inc/hatotach.php
@@ -16,6 +16,11 @@ function my_custom_orderitem_modifier($args) {
         'TQUANT' => (int)$item->get_quantity(),
         'PRICE' => ($line_before_discount + $line_tax) / (int)$item->get_quantity(),
     ];
+// 	echo 'enter simply_modify_orderitem func';
+// 	echo "<pre>";
+// 	print_r($data);
+// 	echo "</pre>";
+	//die();
 
     // Return the modified data
     return ['data' => $data, 'item' => $item];
@@ -24,17 +29,47 @@ function my_custom_orderitem_modifier($args) {
 add_filter('simply_request_data', 'simply_func');
 function simply_func($data)
 {
-    if (isset($data['ORDERITEMS_SUBFORM'])) {
-        unset($data['ORDERITEMS_SUBFORM']);
+	//     if (isset($data['ORDERITEMS_SUBFORM'])) {
+	//         unset($data['ORDERITEMS_SUBFORM']);
+	//     }
+// 	if (isset($data['ORDERITEMS_SUBFORM']) && is_array($data['ORDERITEMS_SUBFORM'])) {
+// 		$data['ORDERITEMS_SUBFORM'] = array_filter($data['ORDERITEMS_SUBFORM'], function($item) {
+// 			return isset($item['PARTNAME']) && $item['PARTNAME'] === '900001';
+// 		});
+
+// 		// Optionally reindex the array to reset keys
+// 		$data['ORDERITEMS_SUBFORM'] = array_values($data['ORDERITEMS_SUBFORM']);
+// 	}
+// 	
+	if (isset($data['ORDERITEMS_SUBFORM']) && is_array($data['ORDERITEMS_SUBFORM'])) {
+        // Loop through and filter items that are NOT WooCommerce products (e.g., shipping lines)
+        $data['ORDERITEMS_SUBFORM'] = array_filter($data['ORDERITEMS_SUBFORM'], function($item) {
+
+            // Check if this partname (SKU) exists as a WooCommerce product
+            $product_id = wc_get_product_id_by_sku($item['PARTNAME']);
+            return empty($product_id); // Keep only if it's NOT a valid product SKU
+        });
+
+        // Reindex array
+        $data['ORDERITEMS_SUBFORM'] = array_values($data['ORDERITEMS_SUBFORM']);
     }
+
     $data['TYPECODE'] = 'WEB';
 	$data['PAYMENTDEF_SUBFORM']['PAYMENTCODE'] = '121';
 		
     $order_id = $data['orderId'];
     $order = new \WC_Order($order_id);
-    $order_cc_meta = $order->get_meta('_transaction_data');
-    $confnum = $order_cc_meta['DebitApproveNumber'];
-    $data['PAYMENTDEF_SUBFORM']['CONFNUM'] = $confnum;
+	$pelecard_transaction_data = get_post_meta( $order_id, '_transaction_data' );
+	foreach ( $pelecard_transaction_data as $order_cc_meta ) {
+	
+		if($order_cc_meta["StatusCode"] != '000'){
+			continue;
+		}
+		//print_r($order_cc_meta);
+		 $confnum = $order_cc_meta['DebitApproveNumber'];
+	}
+   $data['PAYMENTDEF_SUBFORM']['CONFNUM'] = $confnum;
+    
 
     // Return the data
     return $data;
@@ -81,31 +116,76 @@ function simply_syncProspect_func($json_request)
 add_filter('simply_syncItemsPriority_data', 'simply_syncItemsPriority_data_func');
 function simply_syncItemsPriority_data_func($data)
 {
-    $raw_option     = WooAPI::instance()->option( 'sync_items_priority_config' );
+	$raw_option     = WooAPI::instance()->option( 'sync_items_priority_config' );
     $raw_option     = str_replace( array( "\n", "\t", "\r" ), '', $raw_option );
     $config         = json_decode( stripslashes( $raw_option ) );
     $product_price_list = ( ! empty( $config->product_price_list ) ? $config->product_price_list : null );
     $product_price_sale = ( ! empty( $config->product_price_sale ) ? $config->product_price_sale : null );
-
-    $data['expand'] .= ',INTERNALDIALOGTEXT_SUBFORM,PARTINCUSTPLISTS_SUBFORM($select=PLNAME,PRICE,VATPRICE;$filter=PLNAME eq \'' . $product_price_sale . '\')';
+	
 	$data['expand'] = '$expand=INTERNALDIALOGTEXT_SUBFORM,PARTUNSPECS_SUBFORM,PARTTEXT_SUBFORM,PARTINCUSTPLISTS_SUBFORM($select=PLNAME,PRICE,VATPRICE;$filter=(PLNAME eq \'' . $product_price_list . '\' or PLNAME eq \'' . $product_price_sale . '\'))';
-    $data['select'] .= ',SAPL_COOLING,SAPL_ENERGY_RATING,SAPL_HEATING,SAPL_HORSEPOWER';
+	$data['select'] .= ',SAPL_COOLING,SAPL_ENERGY_RATING,SAPL_HEATING,SAPL_HORSEPOWER';
 	return $data;
 }
 
-add_filter('simply_syncPricePriority', 'simply_syncPricePriority_func');
-function simply_syncPricePriority_func($data)
-{
-    $raw_option     = WooAPI::instance()->option( 'sync_items_priority_config' );
-    $raw_option     = str_replace( array( "\n", "\t", "\r" ), '', $raw_option );
-    $config         = json_decode( stripslashes( $raw_option ) );
-    $product_price_list = ( ! empty( $config->product_price_list ) ? $config->product_price_list : null );
-    $product_price_sale = ( ! empty( $config->product_price_sale ) ? $config->product_price_sale : null );
+add_action('simply_update_product_data', function($item){
+    $product_id = $item['product_id'];
+    $horsepower = $item['SAPL_HORSEPOWER'];
+    $cooling = $item['SAPL_COOLING'];
+    $heating = $item['SAPL_HEATING'];
+    $energy_rating = $item['SAPL_ENERGY_RATING'];
+	
+	$short_text = '';
+    if ( isset( $item['INTERNALDIALOGTEXT_SUBFORM'] ) ) {
+        foreach ( $item['INTERNALDIALOGTEXT_SUBFORM'] as $text ) {
+            $clean_text = preg_replace('/<style>.*?<\/style>/s', '', $text);
+            $short_text .= ' ' . html_entity_decode( $clean_text );
+        }
+    }
 
-    //$data['expand'] .= ',INTERNALDIALOGTEXT_SUBFORM,PARTINCUSTPLISTS_SUBFORM($select=PLNAME,PRICE,VATPRICE;$filter=PLNAME eq \'' . $product_price_sale . '\')';
-	$data['expand'] = '$expand=INTERNALDIALOGTEXT_SUBFORM,PARTUNSPECS_SUBFORM,PARTTEXT_SUBFORM,PARTINCUSTPLISTS_SUBFORM($select=PLNAME,PRICE,VATPRICE;$filter=(PLNAME eq \'' . $product_price_list . '\' or PLNAME eq \'' . $product_price_sale . '\'))';
-	return $data;
-}
+    if($product_id !== 0) {
+        update_post_meta( $product_id, 'horsepower', $horsepower );
+        update_post_meta( $product_id, 'cold', $cooling );
+        update_post_meta( $product_id, 'hot', $heating );
+        update_post_meta( $product_id, 'aword', $energy_rating );
+		
+		wp_update_post(array(
+            'ID' => $product_id,
+            'post_excerpt' => $short_text
+        ));
+    }
+	
+	//update INT and INTPRO price list to product
+    $set_tax = get_option('woocommerce_calc_taxes');
+    if ( ! empty( $item['PARTINCUSTPLISTS_SUBFORM'] ) ) {
+        foreach ($item['PARTINCUSTPLISTS_SUBFORM'] as $price_list) {
+            if ( $price_list != null && ! empty( $price_list['PLNAME'] == 'INTPRO') ) {
+                $pri_price = (wc_prices_include_tax() == true || $set_tax == 'no') ? $price_list['VATPRICE'] : $price_list['PRICE'];
+            } 
+            elseif ( $price_list != null && ! empty( $price_list['PLNAME'] == 'INT') ) {
+                $price_sale = (wc_prices_include_tax() == true || $set_tax == 'no') ? $price_list['VATPRICE'] : $price_list['PRICE'];
+            }
+        }
+    }
+	if ( $product_id !== 0 ) {
+        $my_product = wc_get_product( $product_id );
+
+        if ( $my_product ) { // Ensure product object is valid
+            // Update regular price if provided
+            if ( $pri_price != 0 ) {
+                $my_product->set_regular_price( $pri_price ); // Use set_regular_price
+            }
+    
+            // Update sale price if provided
+            if ( $price_sale != 0 ) {
+                $my_product->set_sale_price( $price_sale ); // Use set_sale_price
+            }
+    
+            // Save the product to persist changes
+            $my_product->save();
+        }
+    }
+
+});
 
 //sync all product pricelist int and intpro
 add_action('customSyncPricePriority_cron_hook', 'customSyncPricePriority');
@@ -115,10 +195,6 @@ if (!wp_next_scheduled('customSyncPricePriority_cron_hook')) {
    $res = wp_schedule_event(time(), 'daily', 'customSyncPricePriority_cron_hook');
 
 }
-
-// function customSyncPricePriority(){
-//     WooAPI::instance()->syncPricePriority();
-// }
 
 
 function customSyncPricePriority()
@@ -181,9 +257,6 @@ function customSyncPricePriority()
                 }  
             }
         }
-        // add timestamp
-
-        //$this->updateOption('items_priority_update', time());
     } else {
 
         WooAPI::instance()->sendEmailError(
@@ -199,72 +272,6 @@ function customSyncPricePriority()
     return $response;
 }
 
-
-add_action('simply_update_product_data', function($item){
-    $product_id = $item['product_id'];
-    $horsepower = $item['SAPL_HORSEPOWER'];
-    $cooling = $item['SAPL_COOLING'];
-    $heating = $item['SAPL_HEATING'];
-    $energy_rating = $item['SAPL_ENERGY_RATING'];
-	
-	$short_text = '';
-    if ( isset( $item['INTERNALDIALOGTEXT_SUBFORM'] ) ) {
-        foreach ( $item['INTERNALDIALOGTEXT_SUBFORM'] as $text ) {
-            $clean_text = preg_replace('/<style>.*?<\/style>/s', '', $text);
-            $short_text .= ' ' . html_entity_decode( $clean_text );
-        }
-    }
-
-    if($product_id !== 0) {
-        update_post_meta( $product_id, 'horsepower', $horsepower );
-        update_post_meta( $product_id, 'cold', $cooling );
-        update_post_meta( $product_id, 'hot', $heating );
-        update_post_meta( $product_id, 'aword', $energy_rating );
-		
-		wp_update_post(array(
-            'ID' => $product_id,
-            'post_excerpt' => $short_text
-        ));
-    }
-
-    /*
-    סנכרון מחירונים INTPTI ו INT 
-    עדיין לא הופעל מכיוון שמחכה לעדכון מבני לגבי בעיה עם מחירון INTPRO
-    */
-    //update INT and INTPRO price list to product
-    $set_tax = get_option('woocommerce_calc_taxes');
-    if ( ! empty( $item['PARTINCUSTPLISTS_SUBFORM'] ) ) {
-        foreach ($item['PARTINCUSTPLISTS_SUBFORM'] as $price_list) {
-            if ( $price_list != null && ! empty( $price_list['PLNAME'] == 'INTPRO') ) {
-                $pri_price = (wc_prices_include_tax() == true || $set_tax == 'no') ? $price_list['VATPRICE'] : $price_list['PRICE'];
-            } 
-            elseif ( $price_list != null && ! empty( $price_list['PLNAME'] == 'INT') ) {
-                $price_sale = (wc_prices_include_tax() == true || $set_tax == 'no') ? $price_list['VATPRICE'] : $price_list['PRICE'];
-            }
-        }
-    } /*else {
-        $pri_price = (wc_prices_include_tax() == true || $set_tax == 'no') ? $item['VATPRICE'] : $item['BASEPLPRICE'];
-    }*/
-    if ( $product_id !== 0 ) {
-        $my_product = wc_get_product( $product_id );
-
-        if ( $my_product ) { // Ensure product object is valid
-            // Update regular price if provided
-            if ( $pri_price != 0 ) {
-                $my_product->set_regular_price( $pri_price ); // Use set_regular_price
-            }
-    
-            // Update sale price if provided
-            if ( $price_sale != 0 ) {
-                $my_product->set_sale_price( $price_sale ); // Use set_sale_price
-            }
-    
-            // Save the product to persist changes
-            $my_product->save();
-        }
-    }
-
-});
 
 /**
  * sync inventory from priority
@@ -283,7 +290,7 @@ function syncInventoryPriority()
     $expand = '$expand=LOGCOUNTERS_SUBFORM($select=SAPL_BALANCE),PARTBALANCE_SUBFORM';  
     $data['expand'] = $expand;
 
-    $response = WooAPI::instance()->makeRequest('GET', 'LOGPART?$select='.$data['select'].'&$filter=SHOWINWEB eq \'Y\' &' . $data['expand'], [], WooAPI::instance()->option('log_inventory_priority', false));
+    $response = WooAPI::instance()->makeRequest('GET', 'LOGPART?$select='.$data['select'].'&$filter=SHOWINWEB eq \'Y\' &' . $data['expand'], [], true);
     // check response status
     if ($response['status']) {
         $data = json_decode($response['body_raw'], true);
@@ -340,7 +347,7 @@ function syncInventoryPriority()
 
         }
         // add timestamp
-        //WooAPI::instance()->updateOption('inventory_priority_update', time());
+//         WooAPI::instance()->updateOption('inventory_priority_update', time());
     } else {
         /**
          * t149
@@ -366,13 +373,6 @@ function simply_sync_inventory_priority_func($item)
     return $item;
 }
 
-add_action('simply_after_receipt_func_cron_hook', 'simply_after_receipt_func');
-// Schedule the task to run daily at midnight
-if (!wp_next_scheduled('simply_after_receipt_func_cron_hook')) {
-    $res = wp_schedule_event(time(), 'daily', 'simply_after_receipt_func_cron_hook');
-}
-
-
 function update_receipt_status($ivnum)
 {
     $data = ['STATDES' => "מאושרת"];
@@ -382,18 +382,16 @@ function update_receipt_status($ivnum)
 
     if ($response['status']) {
         $response_data = json_decode($response['body_raw'], true);
-        // updte_post_meta($cprof_id'cprof_status', 'הוזמן מהחנות');
     }
 }
 
-//close receipt
+
 add_filter('simply_after_post_receipt', 'simply_after_receipt_func');
 function simply_after_receipt_func($array)
 {
     // $ord_status = $array["STATDES"];
     $receipt_number = $array["IVNUM"]; 
     $order_id = $array["order_id"];
-    
     update_receipt_status($receipt_number);
     $username = WooAPI::instance()->option('username');
     $password = WooAPI::instance()->option('password');
@@ -436,31 +434,37 @@ function simply_after_receipt_func($array)
     ));
 
     $response = curl_exec($curl);
-    $response_data = json_decode($response, true);
+	$response_data = json_decode($response, true);
+	//wp_mail('elisheva.g@simplyct.co.il','close receipt response data',  json_encode($response_data, JSON_PRETTY_PRINT));
     $res = curl_getinfo($curl);
     if ($res['http_code'] <= 201) {
         if (isset($response_data['ivnum'])) {
-            $order = wc_get_order($order_id);
+			$order = wc_get_order($order_id);
             $order->update_meta_data('priority_recipe_number', $response_data['ivnum']);
             $order->update_meta_data('priority_recipe_status', 'סגורה');
 			$order->save(); 
-            //wp_mail('elisheva.g@simplyct.co.il','success close receipt', $response_data['ivnum']);
+			//wp_mail('elisheva.g@simplyct.co.il','success close receipt for order'.$order_id, $response_data['ivnum']);
+			return;
         }
     }
     else{
         if (isset($response_data['message'])) {
             $msg = $response_data['message'];
         } else {
-            $msg = "No error message found.";
+            $msg = "Failed close receipt.";
         }
-        $order = wc_get_order($order_id);
-        //wp_mail('elisheva.g@simplyct.co.il','close receipt', $msg.' error code'.$res['code']);
-        $order->update_meta_data('priority_recipe_status', $msg);
+		$order = wc_get_order($order_id);
+		$order->update_meta_data('priority_recipe_status', $msg);
+		$order->save();
+        //wp_mail('elisheva.g@simplyct.co.il','failed close receipt', $msg.' error code'.$res['code']);
+		return;
     }
     curl_close($curl);
 
+    return $array;
 }
 
+//on update product, set product status to publish if show in web
 add_action('custom_change_product_status', 'custom_handle_product_status_change');
 
 function custom_handle_product_status_change($product_id) {
